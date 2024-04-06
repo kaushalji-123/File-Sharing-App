608eb623286f0f8d9af14b4982dbe4a96552f632
kaushalji-123 <divyanshu838283fb@gmail.com>
2024-04-06 20:52:50 +0530

    second comit

---
+++ google-meet-clone/context/socket.js
@@ -0,0 +1,14 @@
+import {io} from "socket.io-client";
+
+const SocketProvider = () =>{
+    const [socket, setSocket] = useState(second)
+
+    useEffect(() => {
+      first
+    
+      return () => {
+        second
+      }
+    }, [third])
+    
+}
